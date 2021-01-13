bcddc6ba5af4b8843d0f853e00e8420f48795cd2
Msolc98 <lillit0728@gmail.com>
2021-01-13 23:38:48 +0900

    2021.1.13 first

---
+++ src/file1.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun main() {
+
+    println("hi im testing")
+    val scan:Scanner = Scanner(System.`in`)
+    println("이름을 입력하세요")
+    val name:String = scan.nextLine()
+
+    println("입력하신 이름은 $name 입니다.")
+
+
+}
